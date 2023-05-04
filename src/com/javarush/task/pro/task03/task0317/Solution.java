fabad6f99b175983f57f9c61b222fb6a0b010cca
Vira-Mariia <verpomma123@gmail.com>
2023-05-03 23:47:40 -0400

    level3

---
+++ src/com/javarush/task/pro/task03/task0317/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.pro.task03.task0317;
+
+import java.util.Scanner;
+
+/* 
+Пищевой конвеер
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if (a > 0)
+            System.out.println(a);
+        int b = sc.nextInt();
+        if (b > 0)
+            System.out.println(b);
+        int c = sc.nextInt();
+        if (c > 0)
+            System.out.println(c);
+        int d = sc.nextInt();
+        if (d > 0)
+            System.out.println(d);//напишите тут ваш код
+    }
+}
