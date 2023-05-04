ab3f7a7d5e7ea8e0e2d9a0624bc5802dbe4af691
Vira-Mariia <verpomma123@gmail.com>
2023-05-03 21:03:24 -0400

    init-commit

---
+++ src/com/javarush/task/pro/task02/task0214/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.pro.task02.task0214;
+
+import java.util.Scanner;
+
+/* 
+Чтение и преобразование строк
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+       
+        Scanner scanner = new Scanner(System.in);
+        
+        String str1 = scanner.nextLine();
+        
+        String str2 = scanner.nextLine();
+        
+        String str3 = scanner.nextLine();
+        
+        System.out.println(str3);
+        System.out.println(str2.toUpperCase());
+        System.out.println(str1.toLowerCase());//напишите тут ваш код
+
+    }
+}
